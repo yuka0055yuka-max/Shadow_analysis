880c7570bf2e055ff7785a38ed5c6f87160cc4f6
yuka0055yuka-max <yuka0055yuka@gmail.com>
2025-11-04 02:06:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = 'shadow-pwa-v1';
-const URLS_TO_CACHE = [
-  'image-analyzer.html',
-  'manifest.json'
+const CACHE_NAME = 'shibuya-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './script.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
